730fdcda8fe5c108bd576bb0e1c7b24149550626
JEGA27 <a01658889@itesm.mx>
2021-01-08 09:35:28 -0600

    Mejoras el juego de la vida

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 let tablero;
 let columnas;
 let renglones;
-let celda_tamanio = 10; 
+let celda_tamanio = 10;
+let contador = 0;
+
 
 function setup() {
   createCanvas(600, 400);
@@ -11,40 +13,123 @@ function setup() {
   for(let x = 1; x < columnas-1; x += 1){
     for(let y = 1; y < renglones-1; y += 1){
       tablero[x][y] = floor(random(2));
+      if(random() > 0.85)
+        {
+          tablero[x][y] = 10;
+        }
     }
+
   }
-  //print(tablero);
 }
 
 function draw() {
   background(51);
-  pintaTablero();
-  siguienteGeneracion();
+
+  if (keyIsPressed){
+     if (key=='h'){
+       pintaTablero();
+       siguienteGeneracion();
+     }
+   }
+
+  fill('white');
+  textSize(25);
+  text(contador, 10, 30);
+
 }
 
+
+
 function siguienteGeneracion(){
-  
+
   let tablero_siguiente = crearTablero(columnas, renglones);
   for(let x = 1; x < columnas-1; x += 1){
     for(let y = 1; y < renglones-1; y += 1){
       let celda = tablero[x][y];
       let vecinos = cuentaVecinos(x, y);
-      
+      let cancer = cuentaCancer(x, y);
+
+      //si una celula muerta tiene tres vecinas vivas
+      //nace una viva
       if(celda == 0 && vecinos == 3){
          tablero_siguiente[x][y] = 1;
-      } 
-      
+      }
+
+      //si una celula muerta tiene tres vecinas cancerigenas
+      //nace una cancerigena
+      else if(celda == 0 && cancer == 3){
+         tablero_siguiente[x][y] = 10;
+      }
+
+
+
+      //si una celula viva tiene mas de 3 vecinos o menos de 2 muere
       else if(celda == 1 &&(vecinos > 3 || vecinos < 2)){
         tablero_siguiente[x][y] = 0;
       }
-      
+
+      //si una celula cancerigena tiene
+      //mas de 3 vecinos cancerigenos o menos de 2 muere
+      // == 3 0 > 3
+      else if(celda == 10 &&(cancer == 3 || cancer < 2)){
+        tablero_siguiente[x][y] = 0;
+      }
+
+      //si una celda viva tiene por lo menos a un vecino
+      //cancerigeno se contagia
+      else if(celda == 1 && cancer > 0  ){
+        tablero_siguiente[x][y] = 10;
+      }
+
+
+      //Si una celula viva tiene entre 2 y 3 vecinos vivos se mantiene
       else{
         tablero_siguiente[x][y] = celda;
       }
     }
+    contador += 1;
   }
-  
+
   tablero = tablero_siguiente;
+}
+
+
+function cuentaCancer(x, y){
+
+  let suma_cancer = 0;
+
+  if(tablero[x-1][y-1] == 10){
+    suma_cancer += 1;
+  }
+
+  if(tablero[x][y-1] == 10){
+    suma_cancer += 1;
+  }
+
+  if(tablero[x+1][y-1] == 10){
+    suma_cancer += 1;
+  }
+
+  if(tablero[x-1][y] == 10){
+    suma_cancer += 1;
+  }
+
+  if(tablero[x+1][y] == 10){
+    suma_cancer += 1;
+  }
+
+  if(tablero[x-1][y+1] == 10){
+    suma_cancer += 1;
+  }
+
+  if(tablero[x][y+1] == 10){
+    suma_cancer += 1;
+  }
+  if(tablero[x+1][y+1] == 10){
+    suma_cancer += 1;
+  }
+
+  return suma_cancer;
 }
 
 function cuentaVecinos(x, y){
@@ -66,6 +151,11 @@ function pintaTablero(){
       let posx = x * celda_tamanio;
       let posy = y * celda_tamanio;
       if(tablero[x][y] == 1){
+        fill('blue');
+        stroke(0);
+        rect(posx, posy, celda_tamanio, celda_tamanio);
+      }
+      else if(tablero[x][y] > 1){
         fill('red');
         stroke(0);
         rect(posx, posy, celda_tamanio, celda_tamanio);
